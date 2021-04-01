3d89f084603add35fa3e2b00a00bad74f381a0a1
dsunyou <daniel_li2@brown.edu>
2021-03-31 22:59:49 -0500

    Update submit to increase memory

--- DNN/submit.py
+++ DNN/submit.py
@@ -117,7 +117,7 @@ def submit_job(job):
 Executable = %(RUNDIR)s/remote.sh
 should_transfer_files = YES
 when_to_transfer_output = ON_EXIT
-request_memory = 2.8 GB
+request_memory = 3 GB
 request_cpus = 2
 Output = %(FILENAME)s.out
 Error = %(FILENAME)s.err
