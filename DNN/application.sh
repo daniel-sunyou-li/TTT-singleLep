a01aa334c3c2cfe2bbab858c52376b89a3aebe63
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-04-03 21:22:50 -0400

    Update application.sh

--- DNN/application.sh
+++ DNN/application.sh
@@ -10,10 +10,10 @@ xrdcp -s root://cmseos.fnal.gov//store/user/$eosUserName/CMSSW946.tgz .
 tar -xf CMSSW946.tgz
 rm CMSSW946.tgz
 
-mv *.h5 CMSSW_9_4_6_patch1/src/TTTT_TMVA_DNN/
-mv *.json CMSSW_9_4_6_patch1/src/TTTT_TMVA_DNN/
+mv *.h5 CMSSW_9_4_6_patch1/src/TTT-singleLep/DNN/
+mv *.json CMSSW_9_4_6_patch1/src/TTT-singleLep/DNN/
 
-cd CMSSW_9_4_6_patch1/src/TTTT_TMVA_DNN
+cd CMSSW_9_4_6_patch1/src/TTT-singleLep/DNN/
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 source /cvmfs/sft.cern.ch/lcg/views/LCG_94/x86_64-centos7-gcc8-opt/setup.sh
