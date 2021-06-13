ffdd8ba9f399ef0ec877169e1aaef3497da7a3fd
dsunyou <daniel_li2@brown.edu>
2021-06-13 14:23:43 -0500

    update submit.py

--- DNN/submit.py
+++ DNN/submit.py
@@ -16,16 +16,16 @@ parser.add_argument( "-y",   "--year",        required = True,       help = "The
 parser.add_argument( "-n",   "--seeds",       default = "500",       help = "The number of seeds to submit (only in submit mode)." )
 parser.add_argument( "-c",   "--correlation", default = "60",        help = "The correlation cutoff percentage." )
 parser.add_argument( "-l",   "--varlist",     default = "all",       help = "The variables to use when generating seeds." )
-parser.add_argument( "-nj",  "--njets",       default = "4",         help = "Number of jets to cut on" )
-parser.add_argument( "-nb",  "--nbjets",      default = "2",         help = "Number of b jets to cut on" )
-parser.add_argument( "-ht",  "--ak4ht",       default = "500",       help = "AK4 Jet HT cut" )
-parser.add_argument( "-met", "--met",         default = "60",        help = "MET cut" )
-parser.add_argument( "-lpt", "--leppt",       default = "20",        help = "Lepton PT cut" )
+parser.add_argument( "-nj",  "--NJETS",       default = "4",         help = "Number of jets to cut on" )
+parser.add_argument( "-nb",  "--NBJETS",      default = "2",         help = "Number of b jets to cut on" )
+parser.add_argument( "-ht",  "--AK4HT",       default = "500",       help = "AK4 Jet HT cut" )
+parser.add_argument( "-met", "--MET",         default = "60",        help = "MET cut" )
+parser.add_argument( "-lpt", "--LEPPT",       default = "20",        help = "Lepton PT cut" )
 parser.add_argument( "-mt",  "--MT",          default = "60",        help = "MT of lepton and MET" )
-parser.add_argument( "-j0",  "--jet0pt",      default = "0",         help = "Jet #0 pt cut" )
-parser.add_argument( "-j1",  "--jet1pt",      default = "0",         help = "Jet #1 pt cut" )
-parser.add_argument( "-j2",  "--jet2pt",      default = "0",         help = "Jet #2 pt cut" )
-parser.add_argument( "-dr",  "--minDR",       default = "0.4",       help = "min DR between lepton and jet" )
+parser.add_argument( "-j0",  "--JET0PT",      default = "0",         help = "Jet #0 pt cut" )
+parser.add_argument( "-j1",  "--JET1PT",      default = "0",         help = "Jet #1 pt cut" )
+parser.add_argument( "-j2",  "--JET2PT",      default = "0",         help = "Jet #2 pt cut" )
+parser.add_argument( "-dr",  "--MINDR",       default = "0.4",       help = "min DR between lepton and jet" )
 parser.add_argument(         "--test",        action = "store_true", help = "Only submit one job to test submission mechanics." )
 parser.add_argument(         "--unstarted",   action = "store_true", help = "Include unstarted jobs in the resubmit list." )
 parser.add_argument( "folders", nargs="*",    default = [],          help = "Condor log folders to [re]submit to." ) 
@@ -39,7 +39,7 @@ if args.year not in [ "2016", "2017", "2018" ]:
 # Parse command line arguments
 jt.LOG = args.verbose
 
-# set some parameters
+# set some paraMETers
 step2Sample = config.step2Sample[ args.year ] 
 step2DirLPC = config.step2DirLPC[ args.year ] 
 
@@ -74,16 +74,16 @@ def print_options():
   print( ">> Training Samples: {}".format( step2Sample ) )
   print( ">> Correlation Threshold: {}".format( args.correlation ) ) 
   print( ">> # Seeds: {}".format( args.seeds ) )
-  print( ">> # Jets: {}+".format( args.njets ) )
-  print( ">> # b Jets: {}+".format( args.nbjets ) ) 
-  print( ">> AK4HT: >{}".format( args.ak4ht ) )
-  print( ">> MET: >{}".format( args.met ) )
-  print( ">> Lepton pT: >{}".format( args.leppt ) )
+  print( ">> # Jets: {}+".format( args.NJETS ) )
+  print( ">> # b Jets: {}+".format( args.NBJETS ) ) 
+  print( ">> AK4HT: >{}".format( args.AK4HT ) )
+  print( ">> MET: >{}".format( args.MET ) )
+  print( ">> Lepton pT: >{}".format( args.LEPPT ) )
   print( ">> MET and Lepton MT: >{}".format( args.MT ) )
-  print( ">> Lepton and Jet min(dR): >{}".format( args.minDR ) )
-  print( ">> Jet 0 pT: >{}".format( args.jet0pt ) )
-  print( ">> Jet 1 pT: >{}".format( args.jet1pt ) )
-  print( ">> Jet 2 pT: >{}".format( args.jet2pt ) ) 
+  print( ">> Lepton and Jet min(dR): >{}".format( args.MINDR ) )
+  print( ">> Jet 0 pT: >{}".format( args.JET0PT ) )
+  print( ">> Jet 1 pT: >{}".format( args.JET1PT ) )
+  print( ">> Jet 2 pT: >{}".format( args.JET2PT ) ) 
   print( ">> # Input Variables: {}".format ( len( variables ) ) )
   print( "{} Verbosity".format( "[ON ]" if args.verbose else "[OFF]" ) )
   print( "{} Test".format( "[ON ]" if args.test else "[OFF]" ) )
@@ -120,22 +120,22 @@ def submit_job(job):
   runDir = os.getcwd() 
 # Create a job file
   condorParams = {
-    "MEMORY": "3.8 GB" if int( args.ak4ht ) > 400 else "4.2 GB",
+    "MEMORY": "3.8 GB" if int( args.AK4HT ) > 400 else "4.2 GB",
     "RUNDIR": runDir,
     "FILENAME": job.name,
     "SEEDVARS": seed_vars,
     "EOSUSERNAME": config.eosUserName,
     "YEAR": args.year,
-    "NJETS": args.njets,
-    "NBJETS": args.nbjets,
-    "AK4HT": args.ak4ht,
-    "MET": args.met,
-    "LEPPT": args.leppt,
+    "NJETS": args.NJETS,
+    "NBJETS": args.NBJETS,
+    "AK4HT": args.AK4HT,
+    "MET": args.MET,
+    "LEPPT": args.LEPPT,
     "MT": args.MT,
-    "MINDR": args.minDR,
-    "JET0PT": args.jet0pt,
-    "JET1PT": args.jet1pt,
-    "JET2PT": args.jet2pt
+    "MINDR": args.MINDR,
+    "JET0PT": args.JET0PT,
+    "JET1PT": args.JET1PT,
+    "JET2PT": args.JET2PT
   }
  
   with open( job.path, "w" ) as f:
@@ -245,8 +245,19 @@ def resubmit_jobs():
 # Run in Submit mode
 def submit_new_jobs():
   jf = jt.JobFolder.create(folders[0])
+  jf.year = args.year
+  jf.cuts[ "AK4HT" ] = args.AK4HT
+  jf.cuts[ "NJETS" ] = args.NJETS
+  jf.cuts[ "NBJETS" ] = args.NBJETS
+  jf.cuts[ "MET" ] = args.MET
+  jf.cuts[ "LEPPT" ] = args.LEPPT
+  jf.cuts[ "MT" ] = args.MT
+  jf.cuts[ "MINDR" ] = args.MINDR
+  jf.cuts[ "JET0PT" ] = args.JET0PT
+  jf.cuts[ "JET1PT" ] = args.JET1PT
+  jf.cuts[ "JET2PT" ] = args.JET2PT
   print( ">> Submitting new jobs into folder: {}".format(jf.path))
-  seeds = generate_uncorrelated_seeds( args.seeds, variables, args.correlation, args.year, args.njets, args.nbjets, args.ak4ht, args.leppt, args.met, args.MT, args.minDR, args.jet0Pt, args.jet1Pt, args.jet2Pt )
+  seeds = generate_uncorrelated_seeds( args.seeds, variables, args.correlation, args.year, args.NJETS, args.NBJETS, args.AK4HT, args.LEPPT, args.MET, args.MT, args.MINDR, args.JET0PT, args.JET1PT, args.JET2PT )
 
   print "Generating jobs."
   if jf.jobs == None:
