7c82674527b8f1b5aff8de8c461fa5f9efe8a5bf
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-08-10 17:27:05 -0700

    Create BaseEventSelector.cc

---
+++ FWLJMET/LJMET/plugins/BaseEventSelector.cc
@@ -0,0 +1,41 @@
+#include <math.h>
+#include <boost/algorithm/string.hpp>
+#include "FWLJMET/LJMet/interface/BaseEventSelector.h"
+#include "JetMETCorrections/Objects/interface/JetCorrector.h"
+#include "CondFormats/JetMETObjects/interface/JetCorrectorParameters.h"
+#include "CondFormats/JetMETObjects/interface/FactorizedJetCorrector.h"
+#include "DataFormats/Candidate/interface/Candidate.h"
+
+using namespace std;
+
+BaseEventSelector::BaseEventSelector():
+mName(""),
+mLegend("")
+{
+}
+
+
+void BaseEventSelector::BeginJob(const edm::ParameterSet& iConfig, edm::ConsumesCollector && iC)
+{
+}
+
+void BaseEventSelector::EndJob()
+{
+}
+
+void BaseEventSelector::BeginEvent(edm::EventBase const & event, LjmetEventContent & ec)
+{
+}
+
+
+void BaseEventSelector::EndEvent(edm::EventBase const & event, LjmetEventContent & ec)
+{
+}
+
+
+void BaseEventSelector::Init( void )
+{
+
+    mpEc->SetHistogram(mName, "nEvents", 4, -2,2); // to record total events prior to any selection (and ideally negative weights for MC). 
+
+}
