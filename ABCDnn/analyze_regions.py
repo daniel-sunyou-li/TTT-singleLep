8673d3442ac1e791149dd60a5e2bed23730527b1
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-09-29 19:30:52 -0400

    Create analyze_regions.py

---
+++ ABCDnn/analyze_regions.py
@@ -0,0 +1,21 @@
+# this script takes in multiple ROOT files and parses them for the event multiplicity in a 2D phase space of two control variables
+# last modified September 29, 2021 by Daniel Li
+
+import ROOT
+import numpy as np
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument( "-f", "--files", nargs = "+", required = True, help = "ROOT files to read in" )
+parser.add_argument( "-x1", "--x1", default = "NJets_JetSubCalc", help = "First control variable" )
+parser.add_argument( "-x2", "--x2", default = "NJetsCSV_MultiLepCalc", help = "Second control variable" )
+args = parser.parse_args()
+
+rFiles = {}
+rTrees = {}
+
+for file in args.files:
+  rFiles[ file ] = ROOT.TFile.Open( file )
+  rTrees[ file ] = rFiles[ file ].Get( "Events" )
+  
+
