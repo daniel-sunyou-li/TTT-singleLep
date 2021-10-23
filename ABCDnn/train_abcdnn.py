cf347617bbdb33a8bbf46eefe9bcf402695d6872
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-10-23 17:17:24 -0400

    Update train_abcdnn.py

--- ABCDnn/train_abcdnn.py
+++ ABCDnn/train_abcdnn.py
@@ -2,6 +2,11 @@
 import numpy as np
 from argparse import ArgumentParser
 
+# import custom methods
+import config
+
+nTrans = len( [ var for var in config.variables if config.variables[ var ][ "transform" ] == True )
+
 parser = ArgumentParser()
 parser.add_argument( "-s", "--source", required = True )
 parser.add_argument( "-t", "--target", required = True )
