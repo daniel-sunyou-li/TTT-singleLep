37cd0c4ae17d7f7a13c96ab2af58758beb697f9e
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-10-23 16:34:15 -0400

    Update train_abcdnn.py

--- ABCDnn/train_abcdnn.py
+++ ABCDnn/train_abcdnn.py
@@ -1,1 +1,8 @@
 # updated 10/23 by Daniel Li
+import numpy as np
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument( "-s", "--source", required = True )
+parser.add_argument( "-t", "--target", required = True )
+parser.add_arugment( "-v", "--verbose", store_action = True )
