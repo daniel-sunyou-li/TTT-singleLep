df67839d8e54e37ccbd39ee2a3329c6d8214b62b
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-10-24 00:06:32 -0400

    Update config.py

--- ABCDnn/config.py
+++ ABCDnn/config.py
@@ -52,9 +52,6 @@ regions = {
   }
 }
 
-if int( regions[ "X" ][ "MAX" ] - regions[ "X" ][ "MIN" ] + 1 ) * int( regions[ "Y" ][ "MAX" ] - regions[ "Y" ][ "MIN" ] + 1 ):
-  print( "[WARN] The chosen control variable ranges does not result in 6 regions. Please choose new ranges for control variables X and Y. Edit `regions` in config.py"
-
 params = {
   "EVENTS": {
     "SOURCE": os.path.join( data_path, "Semileptonic_HT0Njet0_2017_mc.root" ),
@@ -74,11 +71,38 @@ params = {
   },
   "TRAIN": {
     "EPOCHS": 15000,
+    "PATIENCE": 5000,
+    "SPLIT": 0.25,
     "MONITOR": 1000,
     "SHOWLOSS": True,
     "SAVEHP": True
   }
 }
+        
+hyper = {
+  "OPTIMIZE": {
+    "NODES_COND": ( [8,16,32,64,128], "CAT" ),
+    "HIDDEN_COND": ( [1,4] ), "INT" ),
+    "NODES_TRANS": ( [1,8,16,32,64,128], "CAT" ),
+    "LRATE": ( [1e-5,1e-4,1e-3,1e-2,1e-1], "CAT" ),
+    "DECAY": ( [1,1e-1,1e-2,1e-3], "CAT" ),
+    "GAP": ( [100,500,1000,5000], "CAT" ),
+    "DEPTH": ( [1,4], "INT" ),
+    "REGULARIZER": ( ["L1","L2","L1+L2","None"], "CAT" ),
+    "ACTIVATION": ( ["swish","relu","elu","softplus"], "CAT" ),
+    "BETA1": ( [0.5,0.75,0.90,0.99,0.999], "CAT" ),
+    "BETA2": ( [0.5,0.75,0.90,0.99,0.999], "CAT" )
+  },
+  "PARAMS": {
+    "PATIENCE": 2000,
+    "EPOCHS": 10000,
+    "N_RANDOM": 20,
+    "N_CALLS": 30,
+    "MINIBATCH": 105,
+    "VERBOSE": True
+  }
+}
+       
 
 
 
