c4468a8b06fa35e9843a2fe516a11bc0da584a80
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-10-25 12:43:42 -0400

    Update train_abcdnn.py

--- ABCDnn/train_abcdnn.py
+++ ABCDnn/train_abcdnn.py
@@ -1,9 +1,14 @@
-# updated 10/23 by Daniel Li
+# updated 10/25 by Daniel Li
 import numpy as np
+import tensorflow as tf
+import tensorflow.keras as keras
+from json import loads as load_json
+from json import dumps as write_json
 from argparse import ArgumentParser
 
 # import custom methods
 import config
+import abcdnn
 
 parser = ArgumentParser()
 parser.add_argument( "-s", "--source", default = "", required = False )
@@ -16,16 +21,51 @@ if args.source != "": config.params[ "EVENTS" ][ "SOURCE" ] = os.path.join( conf
 if args.target != "": config.params[ "EVENTS" ][ "TARGET" ] = os.path.join( config.data_path, args.target )
 
 nTrans = len( [ var for var in config.variables if config.variables[ var ][ "transform" ] == True ] )
-
-space = []
+    
+hp = 
 if args.hpo:
-  for hp in config.hyper[ "OPTIMIZE" ]:
-    if config.hyper[ "OPTIMIZE" ][ hp ][ i ] == "CAT": 
-      space.append( Categorical( config.hyper[ "OPTIMIZE" ][ hp ][0], name = str(hp) ) ) )
-    elif config.hyper[ "OPTIMIZE" ][ hp ][ i ] == "INT":
-      space.append( Integer( config.hyper[ "OPTIMIZE" ][ hp ][0][0], config.hyper[ "OPTIMIZE" ][0][1], name = str(hp) ) )
-    elif config.hyper[ "OPTIMIZE" ][ hp ][ i ] == "REAL":
-      space.append( Real( config.hyper[ "OPTIMIZE" ][ hp ][0][0], config.hyper[ "OPTIMIZE" ][0][1], name = str(hp) ) )
-      
+  print( ">> Running on optimized parameters" )
+  with open( os.path.join( config.results_path, "opt_params.json" ), "r" ) as jsf:
+    
+  for i,
+else:
                
-               
+abcdnn_ = abcdnn.ABCDnn_training()
+abcdnn_.setup_events(
+  rSource = config.params[ "EVENTS" ][ "SOURCE" ], 
+  rTarget = config.params[ "EVENTS" ][ "TARGET" ],
+  selection = config.selection,
+  variables = config.variables,
+  regions = config.regions,
+  mc_weight = config.params[ "EVENTS" ][ "MCWEIGHT" ]
+)
+
+abcdnn_.setup_model(
+  nodes_cond = hp[ "NODES_COND" ],
+  hidden_cond = hp[ "HIDDEN_COND" ],
+  nodes_trans = hp[ "NODES_TRANS" ],
+  lr = hp[ "LRATE" ],
+  decay = hp[ "DECAY" ],
+  gap = hp[ "GAP" ],
+  depth = hp[ "DECAY" ],
+  regularizer = hp[ "REGULARIZER" ],
+  activation = hp[ "ACTIVATION" ],
+  beta1 = hp[ "BETA1" ],
+  beta2 = hp[ "BETA2" ],
+  minibatch = config.params[ "MODEL" ][ "MINIBATCH" ],
+  savedir = config.params[ "MODEL" ][ "SAVEDIR" ],
+  seed = config.params[ "MODEL" ][ "SEED" ],
+  verbose = config.params[ "MODEL" ][ "VERBOSE" ],
+  retrain = config.params[ "MODEL" ][ "RETRAIN" ]
+)
+
+abcdnn_.train(
+  steps = config.params[ "TRAIN" ][ "EPOCHS" ],
+  patience = config.params[ "TRAIN" ][ "PATIENCE" ],
+  monitor = config.params[ "TRAIN" ][ "MONITOR" ],
+  display_loss = config.params[ "TRAIN" ][ "SHOWLOSS" ],
+  save_hp = config.params[ "TRAIN" ][ "SAVEHP" ]
+)
+
+abcdnn_.evaluate_regions()
+abcdnn_.extended_ABCD()
