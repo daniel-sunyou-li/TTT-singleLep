a83784f4a1aefc58289a57232ab2304205517a20
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-12-08 16:46:29 -0500

    Update and rename doCondorTemplates.sh to condor_templates.sh

---
+++ singleLepAnalyzer/makeTemplates/condor_templates.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+condorDir=$PWD
+cmsswBase=$1
+
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+
+# cd $theDir
+# eval `scramv1 runtime -sh`
+
+cd /home/eusai/4t/CMSSW_10_2_16_UL/src
+eval `scramv1 runtime -sh`
+
+cd $theDir
+
+pwd
+python -u hists.py $condorDir \
+  --iPlot=${2} \
+  --region=${3} \
+  --isCategorized=${4} \
+  --year=${5} \
+  --isEM=${6} \
+  --nhott=${7} \
+  --nttag=${8} \
+  --nWtag=${9} \
+  --nbtag=${10} \
+  --njets=${11} \
+  --step1dir=${12} \
