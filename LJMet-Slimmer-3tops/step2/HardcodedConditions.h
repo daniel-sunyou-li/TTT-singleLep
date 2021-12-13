f69fe35434659d75da3cb0861594e98cdb895d65
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-12-13 15:46:27 -0500

    Update HardcodedConditions.h

--- LJMet-Slimmer-3tops/step2/HardcodedConditions.h
+++ LJMet-Slimmer-3tops/step2/HardcodedConditions.h
@@ -18,31 +18,26 @@ public:
     HardcodedConditions();
     ~HardcodedConditions();
 
-    float GetCSVRenormSF(int year, int isE, intnjet, std::string sampleType);
-    float GetDeepJetRenorm2DSF(int nljet, int hjet, std::String sampleType);
     float GetDeepJetRenorm2DSF_HTnj(float HT, int njets, std::string sampleType);
-    // float GetCSVRenorm2DSF_HTnj(float HT, int njets, std::string sampleType);
 
     TFile *tfile_HTNJ_SF;
 
     SFmap hscale_tttw;
     SFmap hscale_tttj;
-    SFmap hscale_ttjj;
-    SFmap hscale_ttbb;
-    SFmap hscale_ttcc;
-    SFmap hscale_tt2b;
-    SFmap hscale_tt1b;
-    SFmap hscale_HT500Njet9_ttjj;
-    SFmap hscale_HT500Njet9_ttbb;
-    SFmap hscale_HT500Njet9_ttcc;
-    SFmap hscale_HT500Njet9_tt2b;
-    SFmap hscale_HT500Njet9_tt1b;
-    SFmap hscale_STs;
-    SFmap hscale_STt;
-    SFmap hscale_STtw;
-    SFmap hscale_WJets;
-
+    // SFmap hscale_ttjj;
+    // SFmap hscale_ttbb;
+    // SFmap hscale_ttcc;
+    // SFmap hscale_tt2b;
+    // SFmap hscale_tt1b;
+    // SFmap hscale_HT500Njet9_ttjj;
+    // SFmap hscale_HT500Njet9_ttbb;
+    // SFmap hscale_HT500Njet9_ttcc;
+    // SFmap hscale_HT500Njet9_tt2b;
+    // SFmap hscale_HT500Njet9_tt1b;
+    // SFmap hscale_STs;
+    // SFmap hscale_STt;
+    // SFmap hscale_STtw;
+    // SFmap hscale_WJets;
 };
-
 
 #endif
