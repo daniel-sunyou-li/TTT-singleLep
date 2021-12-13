6ef7bfe5d72ef7d6947c26809ff527f8cd2f0818
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2021-12-13 16:17:34 -0500

    Update run_step2.py

--- LJMet-Slimmer-3tops/step2/run_step2.py
+++ LJMet-Slimmer-3tops/step2/run_step2.py
@@ -14,7 +14,7 @@ args = parser.parse_args()
 from ROOT import *
 
 if args.year not in [ "16", "17", "18": ]: sys.exit( "[ERR] Invalid year option used. Choose from: 16, 17, 18" )
-shifts = [ "nominal" ] if not args.systematics else [ "nominal", "JECup", "JECdown", "JERup", "JERdown" ]
+shifts = [ "nominal" ] if not args.systematics else [ "JECup", "JECdown", "JERup", "JERdown" ]
 
 relBase = config.relBase
 runDir = os.getcwd()
@@ -70,7 +70,7 @@ Executable = %(RUNDIR)s/make_step2.sh
 Should_Transfer_Files = YES
 WhenToTransferOutput = ON_EXIT
 request_memory = 4096
-Transfer_Input_Files = %(RUNDIR)s/HT_njets_SF_3t.root, %(RUNDIR)s/HardcodedConditions.cc, %(RUNDIR)s/HardcodedConditions.h, %(RUNDIR)s/make_step2.C, %(RUNDIR)s/step2.cc, %(RUNDIR)s/step2.h, %(RUNDIR)s/step2_cc.d, %(RUNDIR)s/step2_cc.so, %(RUNDIR)s/Davismt2.cc, %(RUNDIR)s/Davismt2.h, %(RUNDIR)s/Davismt2_cc.d, %(RUNDIR)s/Davismt2_cc.so
+Transfer_Input_Files = %(RUNDIR)s/renorm/HT_njets_SF_3t_sys.root, %(RUNDIR)s/HardcodedConditions.cc, %(RUNDIR)s/HardcodedConditions.h, %(RUNDIR)s/make_step2.C, %(RUNDIR)s/step2.cc, %(RUNDIR)s/step2.h, %(RUNDIR)s/step2_cc.d, %(RUNDIR)s/step2_cc.so, %(RUNDIR)s/Davismt2.cc, %(RUNDIR)s/Davismt2.h, %(RUNDIR)s/Davismt2_cc.d, %(RUNDIR)s/Davismt2_cc.so
 Output = %(FILENAME)s.out
 Error = %(FILENAME)s.err
 Log = %(FILENAME)s.log
