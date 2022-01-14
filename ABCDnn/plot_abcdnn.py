fbe6b2fd0e3ba46f1823bd156f457535363feded
Daniel Li <45447223+daniel-sunyou-li@users.noreply.github.com>
2022-01-14 12:54:47 -0700

    Delete plot_abcdnn.py

--- ABCDnn/plot_abcdnn.py
+++
@@ -1,7 +0,0 @@
-# plot the various histograms for the mc, raw data and predicted data from ABCDnn, use the trained model
-# last updated 10/25/2021 by Daniel Li
-
-import numpy as np
-from argparse import ArgumentParser
-
-import abcdnn
